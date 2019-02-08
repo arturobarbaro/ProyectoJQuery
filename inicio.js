dbaf858f31e8fb92ab126d98baa2bf4f35867d09
Arturo Barba <arturo.barba@iesdonana.org>
2019-02-08 17:58:44 +0100

    Modificamos apariencia

--- inicio.js
+++ inicio.js
@@ -1,10 +1,23 @@
 function crearPagina(){
-    console.log("Paco cabron saluda al campeon");
+    $('body').append('<div id="div1" ></div>')
+    $('body').append('<br>')
+    $('#div1').append('<h1>Bienvenidos al Gestor de contenido de Arturo</h1>')
+    $('#div1').append('<p>Bienvenidos al Gestor de contenido de Arturo</p>')
+    $('#div1').append('<button id="boton" type="button" name="button">+Info</button>')
+    $('#boton').on('click',function(){
+        $('body').append('<div id="div2" style="background-color:white"></div>')
+        $('#div2').append('<p>Bienvenidos al Gestor de contenido de Arturo</p>')
+        $('#div2').append('<button id="b1" type="button" name="new">Pruebalo</button>')
+    })
 }
 
 function inicio(){
     $(document).off('mousemove');
     window.confirm("¿Desea que aparezca una ayuda?")?crearPagina():alert("De acuerdo. Si desea cambiar de opcion seleccione aceptar");
+}
+
+function mostrarAyuda(){
+
 }
 
 $(document).ready(function(){
