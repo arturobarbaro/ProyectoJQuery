d3149d12080bdf866f18334739628d7b1be77086
Arturo Barba <arturo.barba@iesdonana.org>
2019-03-08 21:11:58 +0100

    Usamos formularios para introducir datos

--- scripts/dom.js
+++ scripts/dom.js
@@ -12,8 +12,7 @@ function insertarElemento(elemento, padre){
 function crearFormularioTitulo(){
     return`<form id="formulario">
         <label>Cadena:</label>
-        <input class="form-control" type="text" id="cadena">
-        <br>
+        <input class="form-control" type="text" id="cadena"><br>
         <label>Tamaño</label>
         <select id="tam">
             <option value="h1">1 (máximo)</option>
@@ -22,21 +21,85 @@ function crearFormularioTitulo(){
             <option value="h4">4</option>
             <option value="h5">5</option>
             <option value="h6">6 (mínimo)</option>
-        </select>
-        <div class="mt-3 text-right">
-          <input class="btn btn-success" id="Enviar" type="submit" value="Enviar" />
-        </div>
+        </select><br>
+        <label>Font-size (em)</label>
+        <input class="form-control" type="number" id="size"><br>
+        <label>Tipo de fuente</label>
+        <select id="tipo">
+            <option value="b">Bold</option>
+            <option value="strong">Importante</option>
+            <option value="i">Italic</option>
+            <option value="em">Emphasized</option>
+            <option value="mark">Marked</option>
+            <option value="small">Small</option>
+            <option value="del">Deleted</option>
+            <option value="ins">Inserted</option>
+            <option value="sub">Subscript</option>
+            <option value="sup">Superscript</option>
+        </select><br>
+        <input class="btn btn-success" id="Enviar" type="submit" value="Enviar" /><br>
+        <input class="btn btn-success" id="Cancelar" type="button" value="Cancelar" /><br>
       </form>`;
 }
 
 function crearFormularioTexto(){
     return`<form id="formulario">
         <label>Cadena:</label>
-        <textarea id="textoT" rows="7" cols="50" name="description" value=""></textarea>
-        <br>
-        <div class="mt-3 text-right">
-          <input class="btn btn-success" id="Enviar" type="submit" value="Enviar" />
-        </div>
+        <textarea id="textoT" rows="7" cols="50" name="description" value=""></textarea><br>
+        <label>Tipo de fuente</label>
+        <select id="tipo">
+            <option value="b">Bold</option>
+            <option value="strong">Importante</option>
+            <option value="i">Italic</option>
+            <option value="em">Emphasized</option>
+            <option value="mark">Marked</option>
+            <option value="small">Small</option>
+            <option value="del">Deleted</option>
+            <option value="ins">Inserted</option>
+            <option value="sub">Subscript</option>
+            <option value="sup">Superscript</option>
+        </select><br>
+        <input class="btn btn-success" id="Enviar" type="submit" value="Enviar" /><br>
+        <input class="btn btn-success" id="Cancelar" type="button" value="Cancelar" /><br>
+      </form>`;
+}
+
+function crearFormularioEnlace(){
+    return`<form id="formulario">
+        <label>Enlace web</label>
+        <input class="form-control" type="text" id="enlace"><br>
+        <label>Cadena que reemplazará al enlace</label>
+        <input class="form-control" type="text" id="cadena"><br>
+        <label>Tipo de fuente</label>
+        <select id="tipo">
+            <option value="b">Bold</option>
+            <option value="strong">Importante</option>
+            <option value="i">Italic</option>
+            <option value="em">Emphasized</option>
+            <option value="mark">Marked</option>
+            <option value="small">Small</option>
+            <option value="del">Deleted</option>
+            <option value="ins">Inserted</option>
+            <option value="sub">Subscript</option>
+            <option value="sup">Superscript</option>
+        </select><br>
+        <input class="btn btn-success" id="Enviar" type="submit" value="Enviar" /><br>
+        <input class="btn btn-success" id="Cancelar" type="button" value="Cancelar" /><br>
+      </form>`;
+}
+
+function crearFormularioImg(){
+    return`<form id="formulario">
+        <label>Elija una imagen</label>
+        <input type="file" id="enlace" name="myFile"><br><br>
+        <label>Cadena alternativa a la imagen</label>
+        <input class="form-control" type="text" id="cadena"><br>
+        <label>Ancho de la imagen (en px)</label>
+        <input class="form-control" type="number" id="ancho" max="500" min="10"><br>
+        <label>Altura de la imagen (en px)</label>
+        <input class="form-control" type="number" max="500" min="10" id="alto"><br>
+        <input class="btn btn-success" id="Enviar" type="submit" value="Enviar" /><br>
+        <input class="btn btn-success" id="Cancelar" type="button" value="Cancelar" /><br>
       </form>`;
 }
 
@@ -44,37 +107,46 @@ function insertarTitulo(e){
     e.preventDefault();
     var d= crearFormularioTitulo();
     $('body').append(d);
-    $('.ocultable').attr('hidden','true');
-    $('#contenido').attr('hidden','true');
+    $('div').attr('hidden','true');
     $('#formulario').css({'border':'1px solid #808080','position': 'absolute', 'background': 'lightblue', 'z-index':'999'});
     $('#Enviar').on('click', function(){
-        var e=`<div><${$('#tam').val()} name="titulo">${$('#cadena').val()}</${$('#tam').val()}></div>`;
+        var e=`<div><${$('#tam').val()} name="titulo"><${$('#tipo').val()}>${$('#cadena').val()}</${$('#tam').val()}><${$('#tipo').val()}></div>`;
         insertarElemento(e,'#contenido');
         $('#formulario').remove();
         $('div').removeAttr('hidden');
     });
+    $('#Cancelar').on('click', function(){$('#formulario').remove();$('div').removeAttr('hidden');})
 }
 
 function insertarTexto(e){
     e.preventDefault();
     var d= crearFormularioTexto();
     $('body').append(d);
-    $('.ocultable').attr('hidden','true');
-    $('#contenido').attr('hidden','true');
+    $('div').attr('hidden','true');
     $('#formulario').css({'border':'1px solid #808080','position': 'absolute', 'background': 'lightblue', 'z-index':'999'});
     $('#Enviar').on('click', function(){
-        var e=`<div><p name="texto">${$('#textoT').children()}</p></div>`;
+        var e=`<div><p name="texto"><${$('#tipo').val()}>${$('#textoT').val()}</${$('#tipo').val()}></p></div>`;
         insertarElemento(e,'#contenido');
         $('#formulario').remove();
         $('div').removeAttr('hidden');
     })
+    $('#Cancelar').on('click', function(){$('#formulario').remove();$('div').removeAttr('hidden');})
 }
 
 function insertarImagen(e){
     e.preventDefault();
-    var ruta=window.prompt('Inserte su ruta');
+    var d= crearFormularioImg();
+    $('body').append(d);
+    $('div').attr('hidden','true');    
+    $('#formulario').css({'border':'1px solid #808080','position': 'absolute', 'background': 'lightblue', 'z-index':'999'});
+    $('#Enviar').on('click', function(){
+        var e=`<div><img src="${$('#enlace').val()}" width="${$('#ancho').val()}px" height="${$('#alto').val()}px" alt="${$('#cadena').val()}"></img></div>`;
+        insertarElemento(e,'#contenido');
+        $('#formulario').remove();
+        $('div').removeAttr('hidden');
+    })
+    $('#Cancelar').on('click', function(){$('#formulario').remove();$('div').removeAttr('hidden');})
     var e=`<div><${this.id} src="${ruta}" width="250px"></${this.id}></div>`;
-    if(ruta){insertarElemento(e,'#contenido');}
 }
 
 function insertarTabla(e){
@@ -111,9 +183,15 @@ function insertarTabla(e){
 
 function insertarRuta(e){
     e.preventDefault();
-    $('#contenido').modal('show');
-    var ruta=window.prompt('Inserte su ruta (http://nombredelsitio.com.ar/archivo.html)');
-    var nombre=window.prompt('Nombre del enlace');
-    var e=`<div><${this.id} href="${ruta}">${nombre}</a></div>`;
-    if(ruta && nombre){insertarElemento(e,'#contenido');}
+    var d= crearFormularioEnlace();
+    $('body').append(d);
+    $('div').attr('hidden','true');    
+    $('#formulario').css({'border':'1px solid #808080','position': 'absolute', 'background': 'lightblue', 'z-index':'999'});
+    $('#Enviar').on('click', function(){
+        var e=`<div><a href="${$('#enlace').val()}" target="_blank"><${$('#tipo').val()}>${$('#cadena').val()}</${$('#tipo').val()}></a></div>`;
+        insertarElemento(e,'#contenido');
+        $('#formulario').remove();
+        $('div').removeAttr('hidden');
+    })
+    $('#Cancelar').on('click', function(){$('#formulario').remove();$('div').removeAttr('hidden');})
 }
